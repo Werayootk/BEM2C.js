88c4f48bc30f45efca4a1f23579a4381bea780b4
Werayoot Kunphai <werayoot.ku@bdms.co.th>
2024-06-20 19:20:43 +0700

    Update Code management cli

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,6 @@ const utils = require('./utils.js');
 
 const yargs = require("yargs");
 
-console.log(yargs.argv);
 if(yargs.argv.version == true){
   utils.showVersion();
   return;
@@ -19,28 +18,43 @@ if (yargs.argv.list == true) {
 
 if (yargs.argv._[0] == null) {
     if (yargs.argv.m === 'forward') {
-        /**
-         * Check -i -o
-         * send path to functionForward(i, o) get result for display success or error 
-         */
         try {
-    
+            if (yargs.argv.i != null && yargs.argv.o != null) {
+                utils.forwardEngineer(yargs.argv.i, yargs.argv.o);
+            } else {
+                console.log("\n" + boxen(chalk.red("\n" + "-i and -o not null." + "\n"), { padding: 1, borderColor: 'red', dimBorder: true, borderStyle: 'classic' }) + "\n");
+                return;
+            }
             console.log("\n" + boxen(chalk.green("\n" + "Forward Method Successfully." + "\n"), { padding: 1, borderColor: 'green', dimBorder: true, borderStyle: 'classic' }) + "\n");
             return;
         } catch (error) {
-            console.log("\n" + boxen(chalk.red("\n" + "Forward Method Failure." + "\n"), { padding: 1, borderColor: 'green', dimBorder: true, borderStyle: 'classic' }) + "\n");
+            console.log("\n" + boxen(chalk.red("\n" + "Forward Method Failure." + "\n"), { padding: 1, borderColor: 'red', dimBorder: true, borderStyle: 'classic' }) + "\n");
             return;
         }
     } else if (yargs.argv.m === 'reverse') {
-        /**
-         * Check -i -o
-         */
-        return;
+        try {
+            if (yargs.argv.i != null && yargs.argv.o != null) {
+                utils.reverseEngineer(yargs.argv.i, yargs.argv.o);
+            } else {
+                console.log("\n" + boxen(chalk.red("\n" + "-i and -o not null." + "\n"), { padding: 1, borderColor: 'red', dimBorder: true, borderStyle: 'classic' }) + "\n");
+                return;
+            }
+            console.log("\n" + boxen(chalk.green("\n" + "Forward Method Successfully." + "\n"), { padding: 1, borderColor: 'green', dimBorder: true, borderStyle: 'classic' }) + "\n");
+            return;
+        } catch (error) {
+            console.log("\n" + boxen(chalk.red("\n" + "Forward Method Failure." + "\n"), { padding: 1, borderColor: 'red', dimBorder: true, borderStyle: 'classic' }) + "\n");
+            return;
+        }
     } else if ((yargs.argv.m != 'reverse' || yargs.argv.m != 'forward') && yargs.argv.m != null) {
         console.log(chalk.red('-m must be "forward" or "reverse"'));
         return;
     }
     utils.showHelp();
     return;
+} else {
+    utils.showHelp();
+    return;
 }
 
+// /Users/werayootk/Downloads/project_code/thesis/BEM2C.js/data/forward/output/
+// /Users/werayootk/Downloads/project_code/thesis/BEM2C.js/data/forward/input/CarRentalSystemDiagram.xmi
--- bin/utils.js
+++ bin/utils.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 const boxen = require('boxen');
 const usage = chalk.hex('#83aaff')("\nUsage: bem2c <command>");
 const packageJson = require('../package.json');
-module.exports = { showHelp: showHelp, showVersion: showVersion, showExample: showExample};
+module.exports = { showHelp: showHelp, showVersion: showVersion, showExample: showExample, forwardEngineer: forwardEngineer, reverseEngineer: reverseEngineer };
 
 function showVersion(){
     console.log(chalk.magenta.bold(`\nVersion :\t\t${packageJson.version}\n`));
@@ -18,9 +18,19 @@ function showHelp() {
     console.log('\t-o\t\t      ' + 'path output.(forward method is path source code but if reverse method is path xmi.)' + '\t\t')
 }
 
- function showExample() {
-    console.log("\n" + boxen(chalk.green("\n" + "Example Forward Method" + "\n"), { padding: 1, borderColor: 'green', dimBorder: true, borderStyle: 'classic' }) + "\n");
-    console.log(chalk.green('bem2c -m forward -i <path_of_xmi> -o <path_of_source_code>\t  '));
-    console.log("\n" + boxen(chalk.red("\n" + "Example Reverse Method" + "\n"), { padding: 1, borderColor: 'red', dimBorder: true, borderStyle: 'classic' }) + "\n");
-    console.log(chalk.red('bem2c -m reverse -i <path_of_source_code> -o <path_of_xmi>\t  '));
- }
+function showExample() {
+console.log("\n" + boxen(chalk.green("\n" + "Example Forward Method" + "\n"), { padding: 1, borderColor: 'green', dimBorder: true, borderStyle: 'classic' }) + "\n");
+console.log(chalk.green('bem2c -m forward -i <path_of_xmi> -o <path_of_source_code>\t  '));
+console.log("\n" + boxen(chalk.red("\n" + "Example Reverse Method" + "\n"), { padding: 1, borderColor: 'red', dimBorder: true, borderStyle: 'classic' }) + "\n");
+console.log(chalk.red('bem2c -m reverse -i <path_of_source_code> -o <path_of_xmi>\t  '));
+}
+
+function forwardEngineer(inputPath, outPath) {
+    console.log("inputPath", inputPath);
+    console.log("outPath", outPath);
+}
+
+function reverseEngineer(inputPath, outPath) {
+    console.log("inputPath", inputPath);
+    console.log("outPath", outPath);
+}
